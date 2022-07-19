90672546ea5490f6a32216c42665330460f2f6b7
RajuJangid <95289422+RajuJangid@users.noreply.github.com>
2022-07-19 23:08:22 +0530

    Update script.js
    updated6.0

--- script.js
+++ script.js
@@ -8,18 +8,13 @@ let w = document.getElementById("won");
 let t = document.getElementById("tie");
 let l = document.getElementById("lost");
 
-function rb(){
-    if(r.style.visibility === 'hidden'){
+function rb() {
+    if (r.style.visibility === 'hidden') {
         r.style.visibility = 'visible';
         p.style.visibility = 'hidden';
-        st.style.visibility = 'hidden';
-        pr.style.visibility = 'hidden';
-        sc.style.visibility = 'hidden';
-        t.style.visibility = 'hidden';
-        w.style.visibility = 'hidden';
-        l.style.visibility = 'hidden';
+
     }
-    else{
+    else {
         r.style.visibility = 'hidden';
         st.style.visibility = 'hidden';
         pr.style.visibility = 'hidden';
@@ -30,12 +25,12 @@ function rb(){
     }
 }
 
-function pb(){
-    if(p.style.visibility === 'hidden'){
+function pb() {
+    if (p.style.visibility === 'hidden') {
         p.style.visibility = 'visible';
         r.style.visibility = 'hidden';
     }
-    else{
+    else {
         p.style.visibility = 'hidden';
         st.style.visibility = 'hidden';
         pr.style.visibility = 'hidden';
@@ -46,101 +41,172 @@ function pb(){
     }
 }
 
-function Computerchooses1(){
-const min = 1;
-const max = 4;
-const intNumber = Math.floor(Math.random() * (max - min)) + min;
-if(intNumber == 1){
-    st.style.visibility = 'visible';
-    pr.style.visibility = 'hidden';
-    sc.style.visibility = 'hidden';
-    t.style.visibility = 'visible';
-    w.style.visibility = 'hidden';
-    l.style.visibility = 'hidden';
-
-}
-else if(intNumber == 2){
-    st.style.visibility = 'hidden';
-    pr.style.visibility = 'visible';
-    sc.style.visibility = 'hidden';
-    l.style.visibility = 'visible';
-    t.style.visibility = 'hidden';
-    w.style.visibility = 'hidden';
-}
-else{
-    st.style.visibility = 'hidden';
-    pr.style.visibility = 'hidden';
-    sc.style.visibility = 'visible';
-    w.style.visibility = 'visible';
-    t.style.visibility = 'hidden';
-    l.style.visibility = 'hidden';
-}
-}
-
-function Computerchooses2(){
+function Computerchooses1() {
+    function hide(){
+        st.style.visibility = 'hidden';
+        pr.style.visibility = 'hidden';
+        sc.style.visibility = 'hidden';
+        t.style.visibility = 'hidden';
+        w.style.visibility = 'hidden';
+        l.style.visibility = 'hidden';
+    }
+    setTimeout(hide,000);
     const min = 1;
     const max = 4;
     const intNumber = Math.floor(Math.random() * (max - min)) + min;
-if(intNumber == 1){
-        st.style.visibility = 'visible';
-        pr.style.visibility = 'hidden';
-        sc.style.visibility = 'hidden';
-        w.style.visibility = 'visible';
-        t.style.visibility = 'hidden';
-        l.style.visibility = 'hidden';
-    
-    }
-    else if(intNumber == 2){
-        st.style.visibility = 'hidden';
-        pr.style.visibility = 'visible';
-        sc.style.visibility = 'hidden';
-        t.style.visibility = 'visible';
-        l.style.visibility = 'hidden';
-        w.style.visibility = 'hidden';
-    }
-    else{
-        st.style.visibility = 'hidden';
-        pr.style.visibility = 'hidden';
-        sc.style.visibility = 'visible';
-        l.style.visibility = 'visible';
-        w.style.visibility = 'hidden';
-        t.style.visibility = 'hidden';
-    }
-}
-
-function Computerchooses3(){
-        const min = 1;
-        const max = 4;
-        const intNumber = Math.floor(Math.random() * (max - min)) + min;        
-        if(intNumber == 1){
+    function SPSChoose() {
+        if (intNumber == 1) {
             st.style.visibility = 'visible';
             pr.style.visibility = 'hidden';
             sc.style.visibility = 'hidden';
-            t.style.visibility = 'hidden';
-            w.style.visibility = 'hidden';
-            l.style.visibility = 'visible';
-        
         }
-        else if(intNumber == 2){
+        else if (intNumber == 2) {
             st.style.visibility = 'hidden';
             pr.style.visibility = 'visible';
             sc.style.visibility = 'hidden';
-            l.style.visibility = 'hidden';
-            t.style.visibility = 'hidden';
-            w.style.visibility = 'visible';
         }
-        else{
+        else {
             st.style.visibility = 'hidden';
             pr.style.visibility = 'hidden';
             sc.style.visibility = 'visible';
+        }
+    }
+    setTimeout(SPSChoose, 900);
+    function result() {
+        if (intNumber == 1) {
             t.style.visibility = 'visible';
             w.style.visibility = 'hidden';
             l.style.visibility = 'hidden';
         }
-    }    
+        else if (intNumber == 2) {
+            l.style.visibility = 'visible';
+            t.style.visibility = 'hidden';
+            w.style.visibility = 'hidden';
+        }
+        else {
+            w.style.visibility = 'visible';
+            t.style.visibility = 'hidden';
+            l.style.visibility = 'hidden';
+        }
+    }
+    setTimeout(result, 2000);
+}
+
+function Computerchooses2() {
+    function hide(){
+        st.style.visibility = 'hidden';
+        pr.style.visibility = 'hidden';
+        sc.style.visibility = 'hidden';
+        t.style.visibility = 'hidden';
+        w.style.visibility = 'hidden';
+        l.style.visibility = 'hidden';
+    }
+    setTimeout(hide,000);
+    const min = 1;
+    const max = 4;
+    const intNumber = Math.floor(Math.random() * (max - min)) + min;
+    function SPSChoose() {
+        if (intNumber == 1) {
+            st.style.visibility = 'visible';
+            pr.style.visibility = 'hidden';
+            sc.style.visibility = 'hidden';
+            w.style.visibility = 'visible';
+            t.style.visibility = 'hidden';
+            l.style.visibility = 'hidden';
+
+        }
+        else if (intNumber == 2) {
+            st.style.visibility = 'hidden';
+            pr.style.visibility = 'visible';
+            sc.style.visibility = 'hidden';
+            t.style.visibility = 'visible';
+            l.style.visibility = 'hidden';
+            w.style.visibility = 'hidden';
+        }
+        else {
+            st.style.visibility = 'hidden';
+            pr.style.visibility = 'hidden';
+            sc.style.visibility = 'visible';
+            l.style.visibility = 'visible';
+            w.style.visibility = 'hidden';
+            t.style.visibility = 'hidden';
+        }
+    }
+    setTimeout(SPSChoose, 900);
+    function result() {
+        if (intNumber == 1) {
+            w.style.visibility = 'visible';
+            t.style.visibility = 'hidden';
+            l.style.visibility = 'hidden';
+        }
+        else if (intNumber == 2) {
+            t.style.visibility = 'visible';
+            l.style.visibility = 'hidden';
+            w.style.visibility = 'hidden';
+        }
+        else {
+            l.style.visibility = 'visible';
+            w.style.visibility = 'hidden';
+            t.style.visibility = 'hidden';
+        }
+    }
+    setTimeout(result, 2000);
+}
 
 
-function rf(){
+function Computerchooses3() {
+    function hide(){
+        st.style.visibility = 'hidden';
+        pr.style.visibility = 'hidden';
+        sc.style.visibility = 'hidden';
+        t.style.visibility = 'hidden';
+        w.style.visibility = 'hidden';
+        l.style.visibility = 'hidden';
+    }
+    setTimeout(hide,000);
+    const min = 1;
+    const max = 4;
+    const intNumber = Math.floor(Math.random() * (max - min)) + min;
+
+    function SPSChoose() {
+        if (intNumber == 1) {
+            st.style.visibility = 'visible';
+            pr.style.visibility = 'hidden';
+            sc.style.visibility = 'hidden';
+        }
+        else if (intNumber == 2) {
+            st.style.visibility = 'hidden';
+            pr.style.visibility = 'visible';
+            sc.style.visibility = 'hidden';
+        }
+        else {
+            st.style.visibility = 'hidden';
+            pr.style.visibility = 'hidden';
+            sc.style.visibility = 'visible';
+        }
+    }
+    setTimeout(SPSChoose, 900);
+    function result() {
+        if (intNumber == 1) {
+            t.style.visibility = 'hidden';
+            w.style.visibility = 'hidden';
+            l.style.visibility = 'visible';
+        }
+        else if (intNumber == 2) {
+            l.style.visibility = 'hidden';
+            t.style.visibility = 'hidden';
+            w.style.visibility = 'visible';
+        }
+        else {
+            t.style.visibility = 'visible';
+            w.style.visibility = 'hidden';
+            l.style.visibility = 'hidden';
+        }
+    }
+    setTimeout(result, 2000);
+}
+
+function rf() {
     location.reload();
-    pb(); 
+    pb();
 }
